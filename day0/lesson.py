153182b51d142f25fc39bb7d33b5d79d83b717d8
Sabiasisaba <githubsaba@gmail.com>
2024-06-13 13:37:18 +0400

    Homework

---
+++ day0/lesson.py
@@ -0,0 +1,30 @@
+from turtle import*
+
+
+
+#we want to Paint a hause
+#step 1: drawa square
+widh(7)
+
+
+
+
+
+left(90)
+
+forward(200)
+left(90)
+left(90)
+
+forward(200)
+left(90)
+
+forward(200)
+left(90)
+
+
+exitonclick()
+
+
+
+
---
+++ day3/homework/level 3.py
@@ -0,0 +1,42 @@
+# print(hello chads)
+# print (12)
+
+# text/ტექსტი = string/სტრინგი
+# number/რიცხვი = integer/ინტეჯერი
+
+
+
+# name = saba
+
+# last_name = buziashvili
+
+# proffesion = web Developer + mentor
+
+# print(name + last_name + proffesion)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# print(10 + 10) # მიმატება
+# print(20 - 10) # გამოკლება
+# print(5 * 5) # გამრავლება
+# print(13 // 2) # გაყოფა
+# print(5 ** 5) # 5 * 5 * 5 * 5 * 5
+# / - გამოიყენება იმისთვის, რომ პასუხი მივიღოთ ათწილადი
+# // - გამოიყენება იმისთვის, რომ პასუხი მივიღოთ მთელი რიცხვი
+
+
+
+# print(10 + 10 + 20 - 10 + 5 * 5 + 10 // 2 + 5 ** 5)
