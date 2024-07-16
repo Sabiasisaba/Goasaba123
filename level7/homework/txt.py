67d02073a7eb491dbf5089764d7dd41afe63e198
Sabiasisaba <githubsaba@gmail.com>
2024-07-16 19:05:40 +0400

    Add files via upload

---
+++ level7/homework/txt.py
@@ -0,0 +1,31 @@
+
+#1
+num1 = int(input("enter fist num: "))
+num2 = int(input("enter second number: "))
+num3 = int(input("enter third number: "))
+num4 = int(input("enter fourth number: "))
+
+print(num1 + num2 - num3 * num4)
+
+#2
+
+mom = int(input("enter moms age: "))
+dad = int(input("enter dads age: "))
+you = int(input("enter your age"))
+
+
+mom_in_20_years = mom + 20 
+
+dad_in_20 = dad + 20
+you_in_20 = you + 20
+
+print(mom_in_20_years)
+print(dad_in_20)
+print(you_in_20)
+
+# 3
+
+name = input("whats your name: ")
+lastname = input("whats your lastname: ")
+
+print("yousers name is",name,"users surname is",lastname)
